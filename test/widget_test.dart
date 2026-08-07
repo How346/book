d9fdfbe14248f1c41567d74f99133259866f5025
How346 <How@pindush.click>
2026-08-07 16:14:07 +0530

    Add files via upload

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,10 +1,11 @@
 import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:ok_book/main.dart'; 
+import 'package:ok_book/main.dart';
 
 void main() {
   testWidgets('App smoke test', (WidgetTester tester) async {
-    await tester.pumpWidget(const ProviderScope(child: OkBookApp()));
-    expect(find.text('OK Book'), findsWidgets);
+    await tester.pumpWidget(const ProviderScope(child: OkBookProApp()));
+    await tester.pumpAndSettle();
+    expect(find.text('OK Book Pro'), findsOneWidget);
   });
 }
